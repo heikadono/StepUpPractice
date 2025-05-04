f9f8e8c41edb30848c948bdb0d01b48069f42924
Tsepenkov Nikita <heikadono@yandex.ru>
2025-05-05 01:13:41 +0500

    finished task_8_15

--- src/main/java/study/stepup/online/main/Main.java
+++ src/main/java/study/stepup/online/main/Main.java
@@ -4,12 +4,25 @@ import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+
 import study.stepup.online.calculations.Fraction;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.pow;
 
 public class Main {
     public static void main(String[] args) {
-        //8.6 Сложение
-        Fraction fraction1 = new Fraction(3, 5);
+        //8.15 Возведение в степень
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int num = parseInt(in.next());
+        System.out.println("Введите степень числа: ");
+        int exp = parseInt(in.next());
+        exponentation( num,  exp);
+
+
+      /*  Fraction fraction1 = new Fraction(3, 5);
         Fraction fraction2 = new Fraction(49, 12);
         Fraction fraction3 = new Fraction(3, 2);
         Fraction fraction4 = new Fraction(1, 3);
@@ -20,15 +33,20 @@ public class Main {
 
         System.out.println(sumAll(numbers1));
         System.out.println(sumAll(numbers2));
-        System.out.println(sumAll(numbers3));
+        System.out.println(sumAll(numbers3)); */
     }
 
-    public static double sumAll (List<Double> numbers) {
+   /* public static double sumAll (List<Double> numbers) {
         double sum =0;
         for (int i=0; i<numbers.size(); i++){
             System.out.println(sum);
             sum+=numbers.get(i);
         }
         return sum;
+    }*/
+
+    public static void exponentation(int num, int exp){
+        System.out.println("Число " + num + " в степени " + exp + " равно " + (int) pow(num, exp));
     }
+
 }
