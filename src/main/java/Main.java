1eab7ca7081b473b0d9ecb157d2aac130f2c5cd6
Tsepenkov Nikita <heikadono@yandex.ru>
2025-05-05 16:39:12 +0500

    task_8_6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,20 +11,21 @@ public class Main {
         Fraction fraction3 = new Fraction(3, 2);
         Fraction fraction4 = new Fraction(1, 3);
 
-        List<Double> numbers1 = new ArrayList<>(Arrays.asList((double) 2, fraction1.doubleValue(), 2.3));
-        List<Double> numbers2 = new ArrayList<>(Arrays.asList(3.6, fraction2.doubleValue(), (double) 3, fraction3.doubleValue()));
-        List<Double> numbers3 = new ArrayList<>(Arrays.asList(fraction4.doubleValue(), (double)1));
+        List<Number> numbers1 = new ArrayList<>(Arrays.asList(1, fraction1.doubleValue(), 2.3));
+        List<Number> numbers2 = new ArrayList<>(Arrays.asList(3.6, fraction2.doubleValue(), 3, fraction3.doubleValue()));
+        List<Number> numbers3 = new ArrayList<>(Arrays.asList(fraction4.doubleValue(), 1));
 
         System.out.println(sumAll(numbers1));
         System.out.println(sumAll(numbers2));
         System.out.println(sumAll(numbers3));
     }
 
-    public static double sumAll (List<Double> numbers) {
+    public static double sumAll (List<Number> numbers) {
         double sum =0;
         for (int i=0; i<numbers.size(); i++){
             System.out.println(sum);
-            sum+=numbers.get(i);
+            double x = (double) numbers.get(i);
+            sum+= x;
         }
         return sum;
     }
